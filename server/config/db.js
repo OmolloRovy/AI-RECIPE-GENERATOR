c5a40f7818a363b357d16b609975997afcfb9235
OmolloRovy <masimevanistelrooy@gmail.com>
2026-05-07 09:42:03 +0300

    ...

--- server/config/db.js
+++ server/config/db.js
@@ -11,3 +11,11 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
+
+pool.on('connect', () => { 
+    console.log('Connected to the database');
+});
+
+pool.on('error', (err) => {
+    console.error('Database connection error:', err);
+});
